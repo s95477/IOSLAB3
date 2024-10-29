2dba727a85fd4e2b618325bf764f48c4018cc739
s95477 <s95477@pollub.edu.pl>
2024-10-29 11:57:21 +0100

    Initial Commit

---
+++ Lab3IOS/Lab3IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab3IOSApp.swift
+//  Lab3IOS
+//
+//  Created by student on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Lab3IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
