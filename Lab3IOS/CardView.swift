060553d212f20465951e8b18ae284f3817bf331c
s95477 <s95477@pollub.edu.pl>
2024-10-29 13:11:21 +0100

    nie dokonczone duplikowanie kart

---
+++ Lab3IOS/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Lab3IOS
+//
+//  Created by student on 29/10/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ Lab3IOS/ThemeButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeButtonView.swift
+//  Lab3IOS
+//
+//  Created by student on 29/10/2024.
+//
+
+import SwiftUI
+
+struct ThemeButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeButtonView()
+}
